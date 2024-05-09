2ff11997d649675c985dfaa654d87cbd5bde87f2
Shivani <sutrejashivani57@gmail.com>
2024-05-09 17:22:55 +0530

    Login Page mobileview

--- client/src/Components/Auth/Login.js
+++ client/src/Components/Auth/Login.js
@@ -1,56 +1,112 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BiUser } from "react-icons/bi";
 import { MdAlternateEmail } from "react-icons/md";
 import { MdLockOutline } from "react-icons/md";
 import { Link } from 'react-router-dom';
+import LoginSvg from '../../assets/LoginSvg';
 function Login() {
+  const [formData, setFormData] = useState({});
+  const [errorMsg, setErrorMsg] = useState("");
+
+  function handleChange(e) {
+    console.log("name: " + e.target.name);
+    console.log("value: " + e.target.value);
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formData);
+  }
   return (
-    <div>
-       <div className='lg:flex'>
-      {/* <div>
-      <img src={wave} className='block border-2 border-pink-600'></img>
+    <div className="flex flex-col h-screen w-screen">
+      <div className="hidden"></div>
+      <div className=" flex-1 flex flex-col ">
+        <div className=" left-0 -z-10  right-0 rounded  ">
+        <LoginSvg />
+        </div>
 
-      </div> */}
-      <div className='border-2'>
-        
+        <div className="-mt-[8vh] sm:-mt-[10vh] md:-mt-[10vh] lg:-mt-[15vh] flex-1 flex flex-col  h-fit">
+          <form
+            className="bg-transparent  h-fit "
+            onSubmit={handleSubmit}
+            
+          >
+            <div className="  flex flex-col justify-center h-fit ">
+              <div className=" py-6 px-10 md:pl-16 lg:pl-20  font-bold text-3xl sm:text-5xl text-black tracking-tight flex   justify-start ">
+                Log in
+              </div>
+              <div className="px-5 py-1 sm:px-14 sm:py-7 md:px-28 md:py-9 lg:px-40 lg:py-9">
+                
+                <div className=" flex my-10 py-3 px-5">
+                  <label htmlFor="email" className="mr-3 ">
+                    <MdAlternateEmail className="text-input text-2xl sm:text-3xl" />
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email ID "
+                    className="border-b-2 text-2xl sm:text-3xl outline-none border-input flex-grow mr-2"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className=" flex my-10 py-3 px-5">
+                  <label htmlFor="password" className="mr-3 ">
+                    <MdLockOutline className="text-input text-2xl sm:text-3xl" />
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Set Password"
+                    className="border-b-2 text-2xl sm:text-3xl outline-none border-input flex-grow mr-2"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className=' px-5 -mt-5 pb-10'>
+                  <span className='text-black font-bold tracking-tight text-lg sm:text-xl cursor-pointer'>
+                      Forgot Password?
+                  </span>
+                </div>
+                <div className="flex justify-center p-2 ">
+                  <button className="bg-button_bg text-button_text text-2xl sm:text-3xl rounded-lg w-full mx-4 py-2 font-semibold">
+                    Login
+                  </button>
+                </div>
+                <div className=" flex items-center  p-2">
+                  <div className="flex-grow h-px bg-gray-400 mx-2"></div>
+                  <span className=" pb-1 text-2xl sm:text-3xl text-gray-500">or</span>
+                  <div className="flex-grow h-px bg-gray-400 mx-2"></div>
+                </div>
+                <div className=" flex justify-center p-2">
+                  <button className="bg-google_bg text-google_text text-2xl sm:text-3xl rounded-lg w-full mx-4 py-2 font-semibold">
+                    Login with Google
+                  </button>
+                </div>
+                <div className=" flex justify-center py-2">
+                  <span className="text-input tracking-tight font-medium text-lg sm:text-xl">
+                    Don't have an account?{" "}
+                    <Link
+                      to="/login"
+                      className="text-black font-semibold tracking-tight text-lg sm:text-xl"
+                    >
+                      Sign up
+                    </Link>
+                  </span>
+                </div>
+              </div>
+            </div>
+            {errorMsg && errorMsg ? (
+              <div className="text-red-500 text-xl m-2">{errorMsg}</div>
+            ) : (
+              <div></div>
+            )}
+          </form>
+        </div>
       </div>
-
-      <div className='border-2 '>
-        <form className='border-2 m-2 border-blue-500 md:border-pink-700 lg:border-green-700 xl:border-black' >
-          <div className='m-4'>
-            <div className='m-2 my-5 font-bold text-2xl text-black tracking-tight'>Log in</div>
-            <div className='m-2'>
-          
-          <div className='flex my-10'>
-            <label htmlFor='email' className='mr-3 '><MdAlternateEmail className='text-input text-2xl'/></label>
-            <input type='email' name='email' id='email' placeholder='Email ID ' className='border-b-2 outline-none border-input flex-grow mr-2'/>
-          </div>
-          <div className='flex my-10'>
-            <label htmlFor='password' className='mr-3 '><MdLockOutline className='text-input text-2xl'/></label>
-            <input type='password' name='password' id='password' placeholder='Set Password' className='border-b-2 outline-none border-input flex-grow mr-2' />
-          </div>
-          <div>
-            <span className='text-black tracking-tight text-lg font-medium cursor-pointer'>Forgot Password? </span>
-          </div>
-          <div className='flex justify-center my-5 '>
-            <button className='bg-button_bg text-button_text text-xl rounded-lg w-full mx-4 py-2 font-semibold' >Log in</button>
-          </div>
-          <div className='flex items-center my-5'>
-            <div className="flex-grow h-px bg-gray-400 mx-2"></div>
-            <span className=" pb-1 text-gray-500">or</span>
-            <div className="flex-grow h-px bg-gray-400 mx-2"></div>
-          </div>
-          <div className='flex justify-center my-5'>
-            <button className='bg-google_bg text-google_text text-xl rounded-lg w-full mx-4 py-2 font-semibold'>Login with Google</button>
-          </div>
-          <div>
-            <span className='text-input tracking-tight text-lg'>Don't have an account? <Link to="/" className='text-black font-medium tracking-tight text-lg'>Log in</Link></span>
-          </div>
-          </div>
-          </div>
-        </form>
-      </div>
-    </div>
     </div>
   )
 }
---
+++ client/src/assets/LoginSvg.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function LoginSvg() {
+  return (
+    <div>
+      <svg
+        width="100%"
+        height="100%"
+        id="svg"
+        viewBox="0 0 1440 690"
+        xmlns="http://www.w3.org/2000/svg"
+        class="transition duration-300 ease-in-out delay-150"
+      >
+        <path
+          d="M 0,700 L 0,262 C 96.25,210.21428571428572 192.5,158.42857142857144 309,143 C 425.5,127.57142857142857 562.25,148.5 678,180 C 793.75,211.5 888.5,253.57142857142856 1012,269 C 1135.5,284.42857142857144 1287.75,273.2142857142857 1440,262 L 1440,700 L 0,700 Z"
+          stroke="none"
+          stroke-width="0"
+          fill="#111111"
+          fill-opacity="1"
+          class="transition-all duration-300 ease-in-out delay-150 path-0"
+          transform="rotate(-180 720 350)"
+        ></path>
+      </svg>
+    </div>
+  );
+}
+
+export default LoginSvg;
